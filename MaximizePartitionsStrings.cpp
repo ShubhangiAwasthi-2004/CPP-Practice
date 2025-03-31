fbffb1557aeabb9ff832e9559608616d88aee19c
ShubhangiAwasthi-2004 <123819563+ShubhangiAwasthi-2004@users.noreply.github.com>
2025-03-31 21:32:19 +0530

    Create MaximizePartitionsStrings.cpp

---
+++ MaximizePartitionsStrings.cpp
@@ -0,0 +1,27 @@
+//Given a string s of lowercase English alphabets, your task is to return the maximum number of substrings formed,
+//after possible partitions (probably zero) of s such that no two substrings have a common character.
+
+class Solution {
+  public:
+    int maxPartitions(string &s) {
+        unordered_map<char, int> lastIndex;
+        int n = s.size();
+        
+        // Store the last occurrence of each character
+        for (int i = 0; i < n; i++) {
+            lastIndex[s[i]] = i;
+        }
+        
+        int partitions = 0, end = 0;
+        
+        for (int i = 0; i < n; i++) {
+            end = max(end, lastIndex[s[i]]); // Expand to the farthest last occurrence
+            if (i == end) { // A valid partition is found
+                partitions++;
+            }
+        }
+        
+        return partitions;
+        
+    }
+};
